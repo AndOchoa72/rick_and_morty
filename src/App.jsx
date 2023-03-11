f0545f67e54feb8d83d0c291591660430a91d256
Andres Ochoa <andochoa@yahoo.com.ar>
2023-03-11 17:21:56 -0300

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 //import logo from './logo.svg';
 import './App.css';
-import Card from './components/Card'
-import Cards from './components/Cards'
-import SearchBar from './components/SearchBar'
+import Card from './components/Card.jsx'
+import Cards from './components/Cards.jsx'
+import SearchBar from './components/SearchBar.jsx'
 import characters, { Rick } from './data.js'
 
 function App() {
